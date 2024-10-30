15430b2e3709b59b1c5ae387a737bc09ec8fd955
juanjoher <juan.hernandez.arenas@correounivalle.edu.co>
2024-10-30 18:28:05 -0500

    conexion a bd

--- VENTANAS_LOGIN_REGISTRARSE/app.js
+++ VENTANAS_LOGIN_REGISTRARSE/app.js
@@ -26,6 +26,24 @@ loginlink.addEventListener("click", () => {
     
 });
 
+password = document.querySelector('#password_r');
+confirm_password = document.querySelector('#confirm_password_r');
+btn_r = document.querySelector('.btn_re');
+confirm_password.addEventListener('input', function(){
+    if(password.value === confirm_password.value){
+        btn_r.disabled = false;
+    }else{
+        btn_r.disabled = true;
+    }
+});
+password.addEventListener('input', function(){
+    if(password.value === confirm_password.value){
+        btn_r.disabled = false;
+    }else{
+        btn_r.disabled = true;
+    }
+});
+
 btn.addEventListener("click", () => {
     fondo.classList.add('active-btn');
     //usuario por defecto
@@ -42,75 +60,75 @@ btn.addEventListener("click", () => {
 iconocerrar.addEventListener("click", () => {
     fondo.classList.remove('active-btn');
 }); 
-document.getElementById('btn_i_s').addEventListener('click', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe por defecto
+// document.getElementById('btn_i_s').addEventListener('click', function(event) {
+//     event.preventDefault(); // Evita que el formulario se envíe por defecto
 
-    // Obtener los valores de los campos del formulario
-    const emailInput = document.querySelector('input[type="email"]').value;
-    const passwordInput = document.getElementById('password-input').value;
+//     // Obtener los valores de los campos del formulario
+//     const emailInput = document.querySelector('input[type="email"]').value;
+//     const passwordInput = document.getElementById('password-input').value;
 
-    // Recuperar los valores almacenados en localStorage
-    const storedEmail = localStorage.getItem('gmail');
-    const storedPassword = localStorage.getItem('password');
+//     // Recuperar los valores almacenados en localStorage
+//     const storedEmail = localStorage.getItem('gmail');
+//     const storedPassword = localStorage.getItem('password');
 
-    // Comparar los valores del formulario con los valores de localStorage
-    if (emailInput === storedEmail && passwordInput === storedPassword) {
-        alert('¡Inicio de sesión exitoso!');
+//     // Comparar los valores del formulario con los valores de localStorage
+//     if (emailInput === storedEmail && passwordInput === storedPassword) {
+//         alert('¡Inicio de sesión exitoso!');
 
-    } else if (emailInput === rgmail && passwordInput === rpassword) {
-        alert('¡Inicio de sesión exitoso!');
+//     } else if (emailInput === rgmail && passwordInput === rpassword) {
+//         alert('¡Inicio de sesión exitoso!');
         
-    } else {
-        alert('Correo electrónico o contraseña incorrectos.');
-    }
-});
-//boton de registro
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Selecciona el botón y los campos por su id
-    const btnRegistrarse = document.getElementById('btn_r');
-    const nombreInput = document.getElementById('nombre_r');
-    const apellidoInput = document.getElementById('apellido_r');
-    const cedulaInput = document.getElementById('cedula_r');
-    const celularInput = document.getElementById('celular_r');
-    const gmailInput = document.getElementById('gmail_r');
-    const passwordInput = document.getElementById('password_r');
-    const confirmPasswordInput = document.getElementById('confir_password_r');
-    const checkbox = document.getElementById('checkbox_r');
-    // Función que se ejecuta cuando se hace clic en el botón
-    btnRegistrarse.addEventListener('click', () => {
-        // Prevenir el envío del formulario si es necesario
+//     } else {
+//         alert('Correo electrónico o contraseña incorrectos.');
+//     }
+// });
+// boton de registro
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     // Selecciona el botón y los campos por su id
+//     const btnRegistrarse = document.getElementById('btn_r');
+//     const nombreInput = document.getElementById('nombre_r');
+//     const apellidoInput = document.getElementById('apellido_r');
+//     const cedulaInput = document.getElementById('cedula_r');
+//     const celularInput = document.getElementById('celular_r');
+//     const gmailInput = document.getElementById('gmail_r');
+//     const passwordInput = document.getElementById('password_r');
+//     const confirmPasswordInput = document.getElementById('confir_password_r');
+//     const checkbox = document.getElementById('checkbox_r');
+//     // Función que se ejecuta cuando se hace clic en el botón
+//     btnRegistrarse.addEventListener('click', () => {
+//         // Prevenir el envío del formulario si es necesario
         
 
-        // Asignar los valores de los campos a las variables globales
-        rnombre = nombreInput.value;
-        rapellido = apellidoInput.value;
-        rcedula = cedulaInput.value;
-        rcelular = celularInput.value;
-        rgmail = gmailInput.value;
-        rpassword = passwordInput.value;
-        rconfirmPassword = confirmPasswordInput.value;
-        raceptaTerminos = checkbox.checked;
+//         // Asignar los valores de los campos a las variables globales
+//         rnombre = nombreInput.value;
+//         rapellido = apellidoInput.value;
+//         rcedula = cedulaInput.value;
+//         rcelular = celularInput.value;
+//         rgmail = gmailInput.value;
+//         rpassword = passwordInput.value;
+//         rconfirmPassword = confirmPasswordInput.value;
+//         raceptaTerminos = checkbox.checked;
         
-        // Guardar los valores en localStorage si la contraseña coincide
-        if (rpassword === rconfirmPassword) {
-            // Guardar los valores en localStorage si las contraseñas coinciden
-            localStorage.setItem('nombre', rnombre);
-            localStorage.setItem('apellido', rapellido);
-            localStorage.setItem('cedula', rcedula);
-            localStorage.setItem('celular', rcelular);
-            localStorage.setItem('gmail', rgmail);
-            localStorage.setItem('password', rpassword);
-            localStorage.setItem('confirmPassword', rconfirmPassword);
-            localStorage.setItem('aceptaTerminos', raceptaTerminos);
+//         // Guardar los valores en localStorage si la contraseña coincide
+//         if (rpassword === rconfirmPassword) {
+//             // Guardar los valores en localStorage si las contraseñas coinciden
+//             localStorage.setItem('nombre', rnombre);
+//             localStorage.setItem('apellido', rapellido);
+//             localStorage.setItem('cedula', rcedula);
+//             localStorage.setItem('celular', rcelular);
+//             localStorage.setItem('gmail', rgmail);
+//             localStorage.setItem('password', rpassword);
+//             localStorage.setItem('confirmPassword', rconfirmPassword);
+//             localStorage.setItem('aceptaTerminos', raceptaTerminos);
     
-            alert('¡Registro completado correctamente!');
-        } else {
-            // Mostrar una alerta si las contraseñas no coinciden
-            alert('Las contraseñas no coinciden. Por favor, verifique e intente de nuevo.');
-        }
+//             alert('¡Registro completado correctamente!');
+//         } else {
+//             // Mostrar una alerta si las contraseñas no coinciden
+//             alert('Las contraseñas no coinciden. Por favor, verifique e intente de nuevo.');
+//         }
 
-    });
-});
+//     });
+// });
 document.getElementById('toggle-password').addEventListener('click', function() {
     const passwordInput = document.getElementById('password-input');
     const icon = this;
@@ -125,4 +143,38 @@ document.getElementById('toggle-password').addEventListener('click', function() 
         icon.classList.remove('fa-lock-open'); // Cambiar al candado cerrado
         icon.classList.add('fa-lock');
     }
+});
+
+
+document.getElementById('toggle-password_r').addEventListener('click', function() {
+    const passwordInput = document.getElementById('password_r');
+    const icon = document.getElementsByClassName('reg')[0];
+
+    // Alternar el tipo de input entre 'password' y 'text'
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; // Mostrar la contraseña
+        icon.classList.remove('fa-lock'); // Cambiar al candado abierto
+        icon.classList.add('fa-lock-open');
+    } else {
+        passwordInput.type = 'password'; // Ocultar la contraseña
+        icon.classList.remove('fa-lock-open'); // Cambiar al candado cerrado
+        icon.classList.add('fa-lock');
+    }
+});
+
+document.getElementById('toggle-password_rc').addEventListener('click', function() {
+    const passwordInput = document.getElementById('confirm_password_r');
+    const icon = document.getElementsByClassName('creg')[0];
+
+    // Alternar el tipo de input entre 'password' y 'text'
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text'; // Mostrar la contraseña
+        icon.classList.remove('fa-lock'); // Cambiar al candado abierto
+        icon.classList.add('fa-lock-open');
+    } else {
+        passwordInput.type = 'password'; // Ocultar la contraseña
+        icon.classList.remove('fa-lock-open'); // Cambiar al candado cerrado
+        icon.classList.add('fa-lock');
+    }
+
 });
--- usuario.js
+++ usuario.js
@@ -1,2 +1,102 @@
-console.log("Hola mundo");
-console.log("Hola mundo");
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const path = require('path');
+
+// Configurar la carpeta de archivos estáticos
+app.use(express.static(path.join(__dirname, 'VENTANAS_LOGIN_REGISTRARSE')));
+app.use(express.static(path.join(__dirname, 'VENTANA_INICIO')));
+
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    database: "japibank",
+    user: "root",
+    password : ""
+});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'VENTANAS_LOGIN_REGISTRARSE', 'index.html'));
+// });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+
+app.post('/validar', (req, res) => {
+    const datos = req.body;
+    let cedula = datos.cedula;
+    let nombre = datos.nombre;
+    let apellido = datos.apellido;
+    let celular = datos.celular;
+    let correo = datos.correo;
+    let password = datos.contrasena;
+
+    let buscar = "SELECT * FROM USUARIO WHERE cedula = "+cedula+"";
+    
+    conexion.query(buscar, (error, result) => {
+        if (error) {
+            throw(error);
+        } else {
+            if (result.length > 0) {
+                res.send('<script>alert("Usuario ya registrado"); window.history.back();</script>');
+            } else {
+                let registrar = "INSERT INTO USUARIO (cedula, nombre, apellido, celular, correo, contrasena) VALUES (" + cedula + ", '" + nombre + "', '" + apellido + "', " + celular + ", '" + correo + "', '" + password + "')";
+
+    
+    conexion.query(registrar, (error, result) => {
+            if (error) {
+                throw(error);
+            } else {
+                console.log('Usuario registrado correctamente');
+            }});
+    }
+
+}})});
+                
+app.post('/iniciar', (req, res) => {
+    const datos1 = req.body;
+    let email = datos1.email_iniciar;
+    let password = datos1.password_input;
+
+    let buscar = "SELECT cedula,correo,contrasena FROM USUARIO WHERE correo = '"+email+"'";
+    let buscar_correcto = "SELECT * FROM USUARIO WHERE correo = '"+email+"' AND contrasena = '"+password+"'";
+    
+
+    conexion.query(buscar, (error, result) => {
+        if (error) {
+            throw(error);
+        } else {
+            if (result.length > 0) {
+                conexion.query(buscar_correcto, (error, result2) => {
+                    if (error) {
+                        throw(error);
+                    } else {
+                        if (result2.length > 0) {
+                            res.send('<script>window.open("ventana_inicio.html", "_blank");</script>');
+                        } else {
+                            res.send('<script>alert("Contraseña incorrecta"); window.history.back();</script>');
+                        }
+                    }
+                });
+            
+            } else {
+                res.send('<script>alert("Usuario no encontrado"); window.history.back();</script>');
+            }
+        }
+    });
+});
+    
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000 http://localhost:3000');
+});
+
